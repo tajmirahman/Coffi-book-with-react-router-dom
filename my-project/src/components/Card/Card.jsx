dfd0dec931b3c5cd5e8141bb4cb8a2939af37f32
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-01 17:50:13 +0600

    use navigate start here

--- my-project/src/components/Card/Card.jsx
+++ my-project/src/components/Card/Card.jsx
@@ -1,14 +1,19 @@
 import PropTypes from 'prop-types';
 
-const Card = ({coffee}) => {
+const Card = ({ coffee }) => {
     // console.log(coffee)
-    const {category,name,description,image,making_process,origin}=coffee;
+    const { category, name, description, image, making_process, origin } = coffee;
     return (
-        <div>
-            <div >
-                <img src={image} alt="" />
-                <h1>{name}</h1>
-
+        <div className="card bg-base-100 hover:shadow-md border-2 border-gray-100">
+            <figure>
+                <img className='h-[300px] w-full object-cover' src={image} />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">{name}</h2>
+                
+                <div className="card-actions justify-end">
+                    <button className="btn btn-primary">Buy Now</button>
+                </div>
             </div>
         </div>
     );
--- my-project/src/components/CoffeeCard/CoffeCard.jsx
+++ my-project/src/components/CoffeeCard/CoffeCard.jsx
@@ -1,37 +1,45 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useNavigate, useParams } from "react-router-dom";
 import Card from "../Card/Card";
 import { useEffect, useState } from "react";
 
 
 
 const CoffeCard = () => {
+    const navigate=useNavigate();
     const data = useLoaderData();
     const { category } = useParams();
 
     const [coffee, setCoffee] = useState([]);
     useEffect(() => {
-        if(category){
+        if (category) {
             if (data && category) {
                 const filterByCategory = [...data].filter(coffee => coffee.category === category);
                 setCoffee(filterByCategory)
             }
         }
-        else{
-            setCoffee(data.slice(0,6));
+        else {
+            setCoffee(data.slice(0, 6));
         }
     }, [data, category]);
 
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <>
 
-            {
-                coffee.length > 0 ?
-                    (coffee.map(coffee => <Card key={coffee.id} coffee={coffee} />))
-                    : (<h1>Category has no found</h1>)
-            }
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
 
-        </div>
+                {
+                    coffee.length > 0 ?
+                        (coffee.map(coffee => <Card key={coffee.id} coffee={coffee} />))
+                        : (<h1>Category has no found</h1>)
+                }
+
+            </div>
+            <div>
+                <button onClick={()=>navigate('/coffee')} className="btn btn-warning mt-6">View All</button>
+            </div>
+
+        </>
     );
 };
 
