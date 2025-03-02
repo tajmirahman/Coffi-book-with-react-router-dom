3edc565e89919608bb270fa0edb5208b95958816
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-03-02 13:59:17 +0600

    local storage done 80%

--- my-project/src/components/Utility/utility.js
+++ my-project/src/components/Utility/utility.js
@@ -1,12 +1,26 @@
-
+// get all favorites
+const getAllFavorites=()=>{
+    const all= localStorage.getItem('favorites');
+    if(all){
+        const favorites= JSON.parse(all);
+        return favorites;
+    }
+    else{
+        return []
+    }
+    
+}
 // add to local storage
 const addFavorite=(coffee)=>{
-    const favorites=[];
+    const favorites=getAllFavorites();
+    const isExits= favorites.find(fav=> fav.id == coffee.id);
+    if(isExits){
+        alert('already added!')
+    }
     favorites.push(coffee);
     localStorage.setItem('favorites',JSON.stringify(favorites));
-
 }
 
 
 
-export {addFavorite}
+export {addFavorite,getAllFavorites}
